02515d80e11f7d7ddc2b936fa01bb74f0ff0414b
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-14 16:56:30 +0300

    added user db context

---
+++ SmartPlate/Data/UserDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SmartPlate.Models;
+
+namespace SmartPlate.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
