732900224d2c03de074f498ae5362152111ca615
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-22 01:48:34 +0300

    added plate response dto

---
+++ SmartPlate/DTOs/Plate/PlateResponseDto.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using SmartPlate.Models.Enums;
+namespace SmartPlate.DTOs.Plate;
+
+namespace SmartPlate.DTOs.Plate
+{
+    public class PlateResponseDto
+    {
+        public Guid Id { get; set; }
+
+        // Core identity
+        public string RegistrationNumber { get; set; } = default!;
+        public PlateType Type { get; set; }
+        public List<PlateCategory> Categories { get; set; } = new();
+
+        // Classification
+        public string Region { get; set; } = default!;
+        public int? YearIssued { get; set; }
+
+        // DVLA rules
+        public bool CanApplyToAnyVehicle { get; set; }
+        public bool IsAssigned { get; set; }
+        public bool AvailableAsCertificate { get; set; }
+
+        public PlateSupplyType Supply { get; set; }
+    }
+}
