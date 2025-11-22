360e06a40163e5462fb41c48a7e9c02ea69c967e
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-22 21:22:43 +0300

    added helper function and resolved issues

--- SmartPlate/Program.cs
+++ SmartPlate/Program.cs
@@ -1,88 +1,113 @@
+
+using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using SmartPlate.Data;
 using SmartPlate.Helpers;
 using SmartPlate.Services.UserService;
 using SmartPlate.Services.PlateService;
-
+using SmartPlate.Services.PlateOwnershipService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT as: Bearer {token}"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 // Bind JwtSettings
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
 
-//db context
+// DbContext
 builder.Services.AddSqlite<AppDbContext>(builder.Configuration.GetConnectionString("AppDb"));
 
-//services
+// Services
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IPlateService, PlateService>();
+builder.Services.AddScoped<IPlateOwnershipService, PlateOwnershipService>();
 
-
-//controllers
+// Controllers
 builder.Services.AddControllers().AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
-
-var key = Encoding.UTF8.GetBytes(jwtSettings.Key);
-
-//authentication middleware.
-builder.Services.AddAuthentication(options =>
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})//JWT Bearer authentication
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    //how JWT tokens are validated
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = jwtSettings.Issuer,
-        ValidateAudience = true,
-        ValidAudience = jwtSettings.Audience,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.FromSeconds(30)
-    };
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 
-    // custom event to allow JWT from cookies if Authorization header is missing
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            if (string.IsNullOrEmpty(context.Request.Headers["Authorization"]))
-            {
-                var cookieName = builder.Configuration["AuthCookie:Name"] ?? "SmartPlateAuth";
-                var token = context.Request.Cookies[cookieName];
-                if (!string.IsNullOrEmpty(token))
-                {
-                    context.Token = token;
-                }
-            }
-            return Task.CompletedTask;
-        }
-    };
+    options.JsonSerializerOptions.Converters.Add(new JsonDateTimeConverter("yyyy-MM-ddTHH:mm:ss"));
+
+
 });
+
+// JWT Authentication
+var key = Encoding.UTF8.GetBytes(jwtSettings.Key);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidateAudience = true,
+            ValidAudience = jwtSettings.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromSeconds(30)
+        };
+
+        // Allow JWT from cookie if header is missing
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                if (string.IsNullOrEmpty(context.Request.Headers["Authorization"]))
+                {
+                    var cookieName = builder.Configuration["AuthCookie:Name"] ?? "SmartPlateAuth";
+                    var token = context.Request.Cookies[cookieName];
+                    if (!string.IsNullOrEmpty(token))
+                        context.Token = token;
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-//Swagger UI 
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -90,9 +115,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 app.Run();
